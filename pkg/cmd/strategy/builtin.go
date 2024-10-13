dd5668ebe3eeb9908fd3f9e8db2e1d8643b0405a
yadi <luoyadi168@hotmail.com>
2024-10-13 20:07:26 +0800

    luobollgrid strategy

--- pkg/cmd/strategy/builtin.go
+++ pkg/cmd/strategy/builtin.go
@@ -28,6 +28,7 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/kline"
 	_ "github.com/c9s/bbgo/pkg/strategy/linregmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/liquiditymaker"
+	_ "github.com/c9s/bbgo/pkg/strategy/luobollgrid"
 	_ "github.com/c9s/bbgo/pkg/strategy/marketcap"
 	_ "github.com/c9s/bbgo/pkg/strategy/pivotshort"
 	_ "github.com/c9s/bbgo/pkg/strategy/pricealert"
